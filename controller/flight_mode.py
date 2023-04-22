4a6ec161de5d0a57d885db00163a638dd7a6df15
Farhad Ibrahimzade <67722444+farhad-ibrahimzade@users.noreply.github.com>
2023-04-21 22:13:38 -0400

    Finishing touches

--- controller/flight_mode.py
+++ controller/flight_mode.py
@@ -3,7 +3,7 @@ from enum import IntEnum
 
 class FlightMode(IntEnum):
     """Stores flight modes for the drone:
-        take_off_and_land  1,
+        takeoff_and_land  1,
         mission  2
     """
 
