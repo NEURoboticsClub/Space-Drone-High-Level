18697cc705687f9cd0b05555f4662f664a8b0d2c
root <root@Ireh>
2024-03-16 11:27:36 -0400

    msg files

---
+++ spacedrone/build/coords/CMakeFiles/coords_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coords_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/coords_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
