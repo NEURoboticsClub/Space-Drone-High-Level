420712a01c9233ebfe62ce88072c50c5399e7839
BeenToJupiter <premskhdwl@gmail.com.com>
2024-06-17 20:54:22 -0400

    publisher script for tags

--- src/yolo_pub/yolo_pub/aruco_publisher.py
+++ src/yolo_pub/yolo_pub/aruco_publisher.py
@@ -4,10 +4,12 @@ and also referred to his YT videos
 '''
 
 #imports
-import cv2.version
 import numpy as np
 import time
 import cv2
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
 
 #dictionary for tags
 ARUCO_DICT = {
@@ -89,53 +91,84 @@ def pose_estimation(corners, marker_size, matrix_coefficients, distortion_coeffi
 
     return rvecs, tvecs, trash
 
-#define the markers to be detected
-aruco_type = "DICT_5X5_100"
-
-arucoDict = cv2.aruco.getPredefinedDictionary(ARUCO_DICT[aruco_type])
-arucoParams = cv2.aruco.DetectorParameters()
-detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
-
-#capture images from the camera
-ip = "http://192.168.1.119:8080/video"		#external camera, use 0 for default camera on the computer
-cap = cv2.VideoCapture(ip)
-
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 500)
-
-#calibration values for the camera
-calibration_matrix = np.array(((1484.001664055822, 0, 945.7762352668258),
-							   (0,1484.001664055822, 520.8557474898012),
-							   (0,0,1)))
-distortion_params = np.array((-0.0179606305852,0.180716809921,-0.00015874041681,-0.00190823724612,-0.333257089362))
 
 
-while cap.isOpened():
-    
-	ret, img = cap.read()
-	h, w, _ = img.shape
-
-	img = cv2.resize(img, (500, 500), interpolation=cv2.INTER_CUBIC)
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)	#convert the image to grayscale for detection
-	corners, ids, rejected = detector.detectMarkers(gray)	#detect
-	marker_size = 100	#define marker size in proper units (subject to change)
-
-	if ids is not None:
-		#obtain rotational and translation vectors wrt tags
-		rvecs, tvecs, _ = pose_estimation(corners, marker_size, calibration_matrix, distortion_params)
+class Publisher(Node):
+	
+    def __init__(self):
+        super().__init__('Publisher')
+        self.publisher = self.create_publisher(Float32, 'tag_poses', 10)
+        timer_period = 2
+        self.timer = self.create_timer(timer_period, self.cb_publish_pose)
 		
-		#draw axes, box and print information of each tag in the frame
-		for i in range(len(ids)):
-			cv2.aruco.drawDetectedMarkers(img, corners)
-			cv2.drawFrameAxes(img, calibration_matrix, distortion_params, rvecs[i], tvecs[i], 5)
-			print(f"ID: {ids[i]}, Rvec: {rvecs[i]}, Tvec: {tvecs[i]}")
+
+    def cb_publish_pose(self):
+		
+        #define the markers to be detected
+        aruco_type = "DICT_5X5_100"
+
+        arucoDict = cv2.aruco.getPredefinedDictionary(ARUCO_DICT[aruco_type])
+        arucoParams = cv2.aruco.DetectorParameters()
+        detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
+
+        #capture images from the camera
+        ip = "http://192.168.1.119:8080/video"		#external camera, use 0 for default camera on the computer
+        cap = cv2.VideoCapture(ip)
+
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 500)
+
+        #calibration values for the camera
+        calibration_matrix = np.array(((1484.001664055822, 0, 945.7762352668258),
+                                    (0,1484.001664055822, 520.8557474898012),
+                                    (0,0,1)))
+        distortion_params = np.array((-0.0179606305852,0.180716809921,-0.00015874041681,-0.00190823724612,-0.333257089362))
+
+
+        while cap.isOpened():
+            
+            ret, img = cap.read()
+            h, w, _ = img.shape
+
+            img = cv2.resize(img, (500, 500), interpolation=cv2.INTER_CUBIC)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)	#convert the image to grayscale for detection
+            corners, ids, rejected = detector.detectMarkers(gray)	#detect
+            marker_size = 100	#define marker size in proper units (subject to change)
+
+            if ids is not None:
+                #obtain rotational and translation vectors wrt tags
+                rvecs, tvecs, _ = pose_estimation(corners, marker_size, calibration_matrix, distortion_params)
+                
+                #draw axes, box and print information of each tag in the frame
+                for i in range(len(ids)):
+                    cv2.aruco.drawDetectedMarkers(img, corners)
+                    cv2.drawFrameAxes(img, calibration_matrix, distortion_params, rvecs[i], tvecs[i], 5)
+                    print(f"ID: {ids[i]}, Rvec: {rvecs[i]}, Tvec: {tvecs[i]}")
+                    
+                    #placeholder for published msgs
+                    msg = Float32()
+                    msg.data = tvecs[0] #just publishing the first element in tvecs
+                    print('Publishing data')
+                    self.publisher.publish(msg)
+                    
+
+            cv2.imshow("Image", img)
+
+            #press q to quit
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
+
+        cv2.destroyAllWindows()
+        cap.release()
+
+def main():
+    rclpy.init()
+    pub = Publisher()
+    rclpy.spin(pub)
     
-	cv2.imshow("Image", img)
+    pub.destroy_node(pub)
+    rclpy.shutdown()
 
-	#press q to quit
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-	    break
-
-cv2.destroyAllWindows()
-cap.release()
+if __name__ == '__main__':
+    main()
