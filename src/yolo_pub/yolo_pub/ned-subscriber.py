a615e8846ee5617a1b2662414b15b103d6a86ffb
Aaron Shao <72823620+shaoaaron@users.noreply.github.com>
2025-02-04 19:54:15 -0500

    create NED Subscriber

---
+++ src/yolo_pub/yolo_pub/ned-subscriber.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Caveat when attempting to run the examples in non-gps environments:
+
+`drone.offboard.stop()` will return a `COMMAND_DENIED` result because it
+requires a mode switch to HOLD, something that is currently not supported in a
+non-gps environment.
+"""
+
+import asyncio
+
+from mavsdk import System
+from mavsdk.offboard import (OffboardError, PositionNedYaw)
