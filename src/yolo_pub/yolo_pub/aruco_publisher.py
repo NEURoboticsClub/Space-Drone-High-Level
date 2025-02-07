b0568935ebd4b4b54b465fb651278c35fdbb2b86
Aaron Shao <72823620+shaoaaron@users.noreply.github.com>
2025-02-06 20:18:39 -0500

    deleted unused bounding box detector

--- src/yolo_pub/yolo_pub/aruco_publisher.py
+++ src/yolo_pub/yolo_pub/aruco_publisher.py
@@ -45,41 +45,6 @@ ARUCO_DICT = {
 	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
 }
  
-#display detecting bounding box
-#never referenced to?
-def aruco_display(corners, ids, rejected, image):
-	if len(corners) > 0:
- 
-		ids = ids.flatten()
- 
-		for (markerCorner, markerID) in zip(corners, ids):
- 
-			corners = markerCorner.reshape((4, 2))
-			(topLeft, topRight, bottomRight, bottomLeft) = corners
- 
-			topRight = (int(topRight[0]), int(topRight[1]))
-			bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-			bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-			topLeft = (int(topLeft[0]), int(topLeft[1]))
- 
-			#draw bounding box lines
-			cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
-			cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
-			cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
-			cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
- 
-			#draw center of the box
-			cX = int((topLeft[0] + bottomRight[0]) / 2.0)
-			cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-			cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
- 
-			#display id number
-			cv2.putText(image, str(markerID),(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-				0.5, (0, 255, 0), 2)
-			print("[Inference] ArUco marker ID: {}".format(markerID))
- 
-	return image
- 
 #calculate pose of the detected tag
 def pose_estimation(corners, marker_size, matrix_coefficients, distortion_coefficients):
  
