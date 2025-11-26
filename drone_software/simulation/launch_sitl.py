86b264f0aecd210535e62b34a19f163753b03a77
aeonshao <shao.aa@northeastern.edu>
2025-11-26 18:59:28 -0500

    added indent

--- drone_software/simulation/launch_sitl.py
+++ drone_software/simulation/launch_sitl.py
@@ -26,7 +26,7 @@ def start_px4_gazebo():
         process.wait()
 
             if process.returncode == 0:
-            print("Simulation started successfully.")
+                print("Simulation started successfully.")
         else:
             print("Simulation fialed with return code:", process.returncode)
             print("Error output:")
